18a50a4a3f45f09faed922f98474e4beff64bd19
AsheleyNascimento <asheleysk8@gmail.com>
2025-05-29 08:27:34 -0300

    repositorio padrão do projeto

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsuarioModule } from './usuario/usuario.module';
+import { MoradorModule } from './morador/morador.module';
+
+@Module({
+  imports: [UsuarioModule, MoradorModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
